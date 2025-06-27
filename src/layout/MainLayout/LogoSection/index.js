f4a6300cfc81e772a5c0bb9b3b3d960102eb7edc
vaishnavi <vaishnavichalsey11@gmail.com>
2025-06-27 17:00:06 +0530

    updated 27.01

--- src/layout/MainLayout/LogoSection/index.js
+++ src/layout/MainLayout/LogoSection/index.js
@@ -1,32 +1,5 @@
-// import { Link } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-
-// // material-ui
-// import { ButtonBase } from '@mui/material';
-
-// // project imports
-// import config from 'config';
-// import Logo from 'ui-component/Logo';
-// import { MENU_OPEN } from 'store/actions';
-
-// // ==============================|| MAIN LOGO ||============================== //
-
-// const LogoSection = () => {
-//   const defaultId = useSelector((state) => state.customization.defaultId);
-//   const dispatch = useDispatch();
-//   return (
-//     <ButtonBase disableRipple onClick={() => dispatch({ type: MENU_OPEN, id: defaultId })} component={Link} to={config.defaultPath}>
-//       <Logo />
-//     </ButtonBase>
-//   );
-// };
-
-// export default LogoSection;
-
 import { useEffect, useState } from 'react';
 
-// import Logo from 'ui-component/HotelCRM-logo.png';
-// import Logo from '../../../assets/images/HMS1.png';
 import { ButtonBase } from '@mui/material';
 import axios from 'axios';
 import Cookies from 'js-cookie';
@@ -34,28 +7,19 @@ import Logo from '../../../assets/images/hostelCRMLogo.png';
 import LogoMain from '../../../assets/images/admin-logo.png';
 
 const LogoSection = () => {
-  // useEffect(() => {
-  //     const HosId = Cookies.get('_Id');
-  //     if (HosId) {
-  //       setHostelId(HosId);
-  //     }
-  //     fetchProfileData(HosId);
-  //   }, [open]);
-  // const hotel = JSON.parse(localStorage.getItem('hotelData'));
-
   const hostelId = Cookies.get('_Id');
 
   const [logoUrl, setLogoUrl] = useState(LogoMain);
-  const baseUrl = process.env.REACT_APP_BACKEND_URL || 'https://smarthostel.samyotech.in';
-  console.log(' baseUrl :', baseUrl);
+  const BaseURL = process.env.REACT_APP_BACKEND_URL;
+  const baseUrl = process.env.REACT_APP_BACKEND_URL_IMG || 'http://localhost:4000';
 
   const fetchHotelData = async () => {
     try {
-      const response = await axios.get(`${baseUrl}/hostel/view/${hostelId}`);
+      const response = await axios.get(`${BaseURL}/hostel/view/${hostelId}`);
 
       const photo = response?.data?.result?.hostelphoto;
+
       const fullImageUrl = photo ? `${baseUrl}${photo}` : LogoMain;
-      console.log('fullImageUrl :', fullImageUrl);
 
       setLogoUrl(fullImageUrl);
     } catch (error) {
